6cc7cd70af448944bde52ff54759130c754e295a
amitkrt10 <46473863+amitkrt10@users.noreply.github.com>
2022-04-19 18:36:50 +0530

    Update git-dep.py

--- git-dep.py
+++ git-dep.py
@@ -3,6 +3,7 @@
 import streamlit as st
 from google.oauth2 import service_account
 from gsheetsdb import connect
+import pandas as pd
 
 # Create a connection object.
 credentials = service_account.Credentials.from_service_account_info(
@@ -24,4 +25,5 @@ def run_query(query):
 sheet_url = st.secrets["private_gsheets_url"]
 rows = run_query(f'SELECT * FROM "{sheet_url}"')
 
-st.write(list(rows))
+df = pd.read_json(rows)
+st.write(df)
