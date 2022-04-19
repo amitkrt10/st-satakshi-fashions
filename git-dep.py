a37b0b1e7c686025f97d64a5634f4d2b54cbfd66
amitkrt10 <46473863+amitkrt10@users.noreply.github.com>
2022-04-19 18:15:50 +0530

    Update git-dep.py

--- git-dep.py
+++ git-dep.py
@@ -21,9 +21,7 @@ def run_query(query):
     rows = rows.fetchall()
     return rows
 
-sheet_url = st.secrets["private_gsheets_url"]
+sheet_url = st.secrets["https://docs.google.com/spreadsheets/d/1XTagWQZDX76UgMGylmMb99QGbGSD_myrPqaAfxQluPo/edit#gid=0"]
 rows = run_query(f'SELECT * FROM "{sheet_url}"')
 
-# Print results.
-for row in rows:
-    st.write(f"{row.name} has a :{row.pet}:")
+st.write(rows)
