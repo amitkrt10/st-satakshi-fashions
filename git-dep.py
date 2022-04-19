1c99d49886000ccec6f45bcccdbb6100f8e256a2
amitkrt10 <46473863+amitkrt10@users.noreply.github.com>
2022-04-19 18:37:55 +0530

    Update git-dep.py

--- git-dep.py
+++ git-dep.py
@@ -25,5 +25,5 @@ def run_query(query):
 sheet_url = st.secrets["private_gsheets_url"]
 rows = run_query(f'SELECT * FROM "{sheet_url}"')
 
-df = pd.read_json(rows)
-st.write(df)
+#df = pd.read_json(rows)
+st.write(type(rows))
